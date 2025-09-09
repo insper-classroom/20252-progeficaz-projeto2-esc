6813912cf7451f3cf3ba19cd44043db2d71c0ce9
Nader Taha <tahanader068@gmail.com>
2025-09-09 13:08:29 -0300

    save

--- servidor.py
+++ servidor.py
@@ -25,31 +25,7 @@ def imoveis():
     resp, status = get_data()
     return resp, status
 
-# @app.route('/imoveis/<int:id>', methods=['GET'])
-# def imovel_detail(id):
-#     conn = connect_db()
-#     if conn is None:
-#         return {"erro": "Erro ao conectar ao banco de dados"}, 500
-    
-#     cursor = conn.cursor()
-#     sql = "SELECT * FROM imoveis WHERE id = %s"
-#     cursor.execute(sql, (id,))
-#     result = cursor.fetchone()
-#     if not result:
-#         return {"erro": "Imóvel não encontrado"}, 404
-#     else:
-#         imovel_dict = {
-#             "id": result[0],
-#             "logradouro": result[1],
-#             "tipo_logradouro": result[2],
-#             "bairro": result[3],
-#             "cidade": result[4],
-#             "cep": result[5],
-#             "tipo": result[6],
-#             "valor": float(result[7]),
-#             "data_aquisicao": result[8]
-#         }
-#         return imovel_dict, 200
+
 
 
 if __name__ == "__main__":
